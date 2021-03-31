64c64777555bd7f589468f62cc656bb4a2c8c66d
Skorpion_Nabeel <nabeeldesigner91@hotmail.com>
2021-04-01 00:30:50 +0530

    :art: Added more markup and styles
    app.js has been updated but the code needs imporvement

---
+++ app.js
@@ -0,0 +1,12 @@
+// const listItems = document.querySelectorAll(".sidebar-menu-items");
+
+// listItems.forEach((eachLi) => {
+//    eachLi.addEventListener("click", (e) => {
+//       e.preventDefault();
+
+//       if (e.target.classList.contains("current") === false) {
+//          console.log("oof");
+//          e.target.classList.toggle("current");
+//       }
+//    });
+// });
