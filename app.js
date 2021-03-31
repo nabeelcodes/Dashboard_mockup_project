c14faaab70559e86771192b342db35934c5568f1
Skorpion_Nabeel <nabeeldesigner91@hotmail.com>
2021-04-01 03:09:40 +0530

    :recycle: Added correct logic
    updated app.js with correct logic and refactored code for efficiency; also updated markup and stylesheet

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-// const listItems = document.querySelectorAll(".sidebar-menu-items");
+window.addEventListener("DOMContentLoaded", (e) => {
+   e.preventDefault();
 
-// listItems.forEach((eachLi) => {
-//    eachLi.addEventListener("click", (e) => {
-//       e.preventDefault();
+   // Sidebar Menu Item set to class=".current" on Click
+   const listItems = document.querySelectorAll(".sidebar-menu-item");
 
-//       if (e.target.classList.contains("current") === false) {
-//          console.log("oof");
-//          e.target.classList.toggle("current");
-//       }
-//    });
-// });
+   listItems.forEach((eachListItem) => {
+      eachListItem.addEventListener("click", function setsCurrent() {
+         // first remove .current from all <li>s
+         listItems.forEach((eachListItem) => {
+            eachListItem.classList.remove("current");
+         });
+         // then add .current to the current clicked <li>
+         this.classList.add("current");
+      });
+   });
+});
