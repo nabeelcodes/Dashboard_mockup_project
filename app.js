b9697ae08a559eb0b42afa5ed9275e1e2607ed53
Skorpion_Nabeel <nabeeldesigner91@hotmail.com>
2021-04-03 01:04:24 +0530

    :art: Added Sidebar collapse functionality
    - Completed markup and style for Customer section
    - Added transitions for sidebar collapse and sidebar collapse button
    - Added transition effects for search-bar-icon, cards and buttons

--- app.js
+++ app.js
@@ -14,4 +14,27 @@ window.addEventListener("DOMContentLoaded", (e) => {
          this.classList.add("current");
       });
    });
+
+   // Sidebar collapse functionality
+   const chevronButton = document.querySelector(".before-collapse");
+   const sidebar = document.querySelector("#sidebar");
+   const mainSection = document.querySelector("#main");
+
+   function collapsedState() {
+      chevronButton.classList.remove("before-collapse");
+      chevronButton.classList.add("after-collapse");
+      sidebar.classList.add("sidebar-collapsed");
+      mainSection.classList.add("main-section-expanded");
+   }
+
+   function expandedState() {
+      chevronButton.classList.add("before-collapse");
+      chevronButton.classList.remove("after-collapse");
+      sidebar.classList.remove("sidebar-collapsed");
+      mainSection.classList.remove("main-section-expanded");
+   }
+
+   chevronButton.addEventListener("click", () => {
+      chevronButton.classList.contains("before-collapse") ? collapsedState() : expandedState();
+   });
 });
